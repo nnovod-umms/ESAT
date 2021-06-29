779a2ecbfe80faf84809137a8d0443aee9dadbde
nnovod-umms <nathaniel.novod@gmail.com>
2021-06-28 22:47:11 -0400

    Make file fold

--- src/main/scala/edu/umms/ESAT/utils/ReaderWithHeader.scala
+++ src/main/scala/edu/umms/ESAT/utils/ReaderWithHeader.scala
@@ -19,54 +19,56 @@ object ReaderWithHeader {
    */
   def foldFile[T]
   (
-    file: File, headersNeeded: List[String],
+    file: File,
+    headersNeeded: List[String],
     init: T,
     doFold: (T, Array[String], Map[String, Int]) => T,
     sep: String = "\t"
-  ): T | Error = {
+  ): T | Error =
+  {
     // Get path for error messages
     val path = file.getCanonicalPath
-    // Go process file (using will close it)
-    Control.using(Source.fromFile(file)) {
-      input =>
-        // Get reader to iterate over lines
-        val reader = input.getLines()
-        // Look for header
-        reader.nextOption() match {
-          case None =>
-            Error(s"No header found in $path")
-          case Some(header) =>
-            // Get headers and make it into map of (headerName->index)
-            val headers = header.split(sep)
-            val headersMap = headers.zipWithIndex.toMap
-            // Make sure all wanted headers are there
-            headersNeeded.find(wantedHeader => !headersMap.exists(_._1 == wantedHeader)) match {
-              case Some(headerMissing) =>
-                Error(s"$path missing mandatory header $headerMissing")
-              case None =>
-                // Do fold
-                try {
-                  @tailrec
-                  /**
+    try {
+      // Go process file (using will close it)
+      Control.using(Source.fromFile(file)) {
+        input =>
+          // Get reader to iterate over lines
+          val reader = input.getLines()
+          // Look for header
+          reader.nextOption() match {
+            case None =>
+              Error(s"No header found in $path")
+            case Some(header) =>
+              // Get headers and make it into map of (headerName->index)
+              val headers = header.split(sep)
+              val headersMap = headers.zipWithIndex.toMap
+              // Make sure all wanted headers are there
+              headersNeeded.find(wantedHeader => !headersMap.exists(_._1 == wantedHeader)) match {
+                case Some(headerMissing) =>
+                  Error(s"$path missing mandatory header $headerMissing")
+                case None =>
+                  /*
                    * Recursive fold to callback to fold in lines as we parse them.
                    * @param input remaining input
                    * @param soFar results of fold so far
                    * @return fold result
                    */
+                  @tailrec
                   def fold(input: Iterator[String], soFar: T): T = {
                     input.nextOption() match {
                       case None => soFar
                       case Some(line) =>
-                        fold(input, doFold(soFar, line.split(sep), headersMap))
+                        val next = doFold(soFar, line.split(sep), headersMap)
+                        fold(input, next)
                     }
                   }
-                  // Go do fold
+                  // Do fold
                   fold(reader, init)
-                } catch {
-                  case e => Error( s"Error processing input from file $path: ${e.getLocalizedMessage}")
-                }
-            }
-        }
+              }
+          }
+      }
+    } catch {
+      case e => Error(s"Error processing input from file $path: ${e.getLocalizedMessage}")
     }
   }
 }
