df9095b62c70d75f2528e498fe0461a634bd58ae
nnovod-umms <nathaniel.novod@gmail.com>
2021-07-14 22:28:28 -0400

    Fix exon settings for BED file

--- src/main/scala/edu/umms/ESAT/bio/GeneBEDAnnotation.scala
+++ src/main/scala/edu/umms/ESAT/bio/GeneBEDAnnotation.scala
@@ -9,14 +9,14 @@ import java.io.File
 object GeneBEDAnnotation {
   // Get logger
   lazy private val logger: Logger = LogManager.getLogger(this.getClass.getName)
-  // Make symbols for indicies to pieces of input line - must be in order fields will appear in lines in file
+  // Make symbols for indicies to pieces of input line - fields must be in order they will appear in lines in file
   private val (
     chrom: Int, txStart: Int, txEnd: Int,
     geneSymbol: Int, bedScore: Int,
     orientation: Int, cdsStart: Int, cdsEnd: Int,
     pslString: Int, numExons: Int,
     exonSizes: Int, exonStarts: Int
-    ) = Tuple.fromArray((0 to 9).toArray)
+    ) = Tuple.fromArray((0 to 11).toArray)
 
   /**
    * Fold BED annotation file into chosen object.
@@ -54,12 +54,13 @@ object GeneBEDAnnotation {
                     getField(numExons).flatMap(numberExons =>
                       getIntFields(exonSizes).flatMap(exonSizesArray =>
                         getIntFields(exonStarts).map(exonStartArray => {
-                          val (exonStarts, exonEnds) =
-                            getExonStartsAndEnds(blockStarts = exonStartArray, blockSizes = exonSizesArray,
-                              size = numberExons.toInt, start = txStartStr.toInt)
                           // We retrieved all the fields needed - make a Gene and callback to fold it in
+                          val start = txStartStr.toInt
+                          val (exonStarts, exonEnds) =
+                            getExonStartsAndEnds(exonStartOffsets = exonStartArray, exonSizes = exonSizesArray,
+                              numExons = numberExons.toInt, blockStart = start)
                           val newGene = Gene(
-                            chr = chr, start = txStartStr.toInt, end = txEndStr.toInt,
+                            chr = chr, start = start, end = txEndStr.toInt,
                             name = geneName, orientation = strand,
                             exonStarts = exonStarts, exonEnds = exonEnds
                           )
@@ -86,17 +87,29 @@ object GeneBEDAnnotation {
     }
   end foldAnnotation
 
-  private def getExonStartsAndEnds(blockStarts: Array[Int], blockSizes: Array[Int], size: Int, start: Int)
+  /**
+   * Get lists for exon starts/ends.
+   * @param exonStartOffsets offsets from block start to exons
+   * @param exonSizes sizes of exons
+   * @param numExons # of exons
+   * @param blockStart block start
+   * @return (exonStarts, exonEnds)
+   */
+  private def getExonStartsAndEnds(exonStartOffsets: Array[Int], exonSizes: Array[Int], numExons: Int, blockStart: Int)
   : (List[Int], List[Int]) =
+    // Helper to make an integer from a string
+    // @TODO replaceAll needed?  Was in setBlockStartsAndEnds of old Gene code
     def makeInt(s: String) = Integer.parseInt(s.replaceAll("\"", "").trim)
 
-    val numExons = Math.min(size, Math.min(blockSizes.size, blockStarts.size))
-    blockStarts.slice(0, numExons).indices.foldLeft((List.empty[Int], List.empty[Int])) {
+    // Get maximum size we can use
+    val exonsToProcess = Math.min(numExons, Math.min(exonSizes.size, exonStartOffsets.size))
+    // Go through arrays and map them to lists of exon starts and ends
+    exonStartOffsets.slice(0, exonsToProcess).indices.foldLeft((List.empty[Int], List.empty[Int])) {
       case ((starts, ends), index) =>
-        val blockStart = start + blockStarts(index)
-        (blockStart +: starts, (blockStart + blockSizes(index)) +: ends)
+        // Get starting address of exon
+        val exonStart = blockStart + exonStartOffsets(index)
+        // Set exonstart and end into lists
+        (exonStart +: starts, (exonStart + exonSizes(index)) +: ends)
     }
   end getExonStartsAndEnds
-
-
 }
